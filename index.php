7e59f2a8d6052ffd822f78122a76b433e43a9e36
ilovefox8 <ilovefox8@gmail.com>
2015-12-08 15:37:18 +0800

    index

--- index.php
+++ index.php
@@ -3,11 +3,6 @@ include "bootstrap.php";
 
 $app = new \Fox\Core\Application();
 
-if (!$app->isInstalled()) {
-    header("Location: install/");
-    exit;
-}
-
 if (!empty($_GET['entryPoint'])) {
     $app->runEntryPoint($_GET['entryPoint']);
     exit;
